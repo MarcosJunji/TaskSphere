8a622794ecda3a8e12a810ac41f6d0725df305cb
MarcosJunji <marcosjunji20@gmail.com>
2024-09-14 17:04:45 -0300

    Final Commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/TaskSphere"
-})
+  base: "/TaskSphere",
+  server: {
+    host: true, 
+    port: 80
+  }
+});
